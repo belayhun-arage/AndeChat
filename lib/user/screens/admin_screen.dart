65702cd0ab12507464159c94fa3b5b50bcd5c611
BELAY-hun <56740469+BELAY-hun@users.noreply.github.com>
2021-02-28 04:43:30 +0300

    working on admin role

---
+++ lib/user/screens/admin_screen.dart
@@ -0,0 +1,55 @@
+import 'package:ChatUI/libs.dart';
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatefulWidget {
+  static const String Route = "/admin";
+  @override
+  _AdminScreenState createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  String appname = "AdminPage";
+
+  int sceen = 0;
+
+  List<Widget> authentications;
+  @override
+  void initState() {
+    setState(() {
+      authentications = [
+        AuthLogin(
+          function :setAuthorization ,
+        ),
+        AuthRegistration(
+          function: setAuthorization,
+        ),
+      ];
+    });
+    super.initState();
+
+  }
+
+  setAuthorization(int val) {
+    if (val < 2 && val > -1) {
+      setState(() {
+        sceen = val;
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: authentications[sceen],
+      // body: AuthRegistration(),
+    );
+  }
+}
