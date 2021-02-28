aaaf7e860227b8a02a018813a3ff351eb79c0241
FANIMAN <fantahunfekadu1@gmail.com>
2021-03-01 00:06:52 +0300

    admin structure

---
+++ lib/admin/widgets/logout_fragment.dart
@@ -0,0 +1,12 @@
+ import 'package:flutter/material.dart';
+
+
+class LogoutFragment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Center(
+      child: new Text("You are Logging Out"),
+    );
+  }
+}
---
+++ lib/admin/widgets/search_fragment.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+
+class SearchFragment extends StatelessWidget {
+  bool isLoading = false;
+  bool searchEntryIsVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          autofocus: true,
+          cursorColor: Colors.white,
+          showCursor: true,
+          decoration: InputDecoration(
+            hintStyle: TextStyle(
+              color: Colors.white,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+            ),
+            counterStyle: TextStyle(
+              color: Colors.white24,
+            ),
+            focusColor: Colors.white,
+            hintText: "    Type Username .. ",
+            alignLabelWithHint: true,
+            helperMaxLines: 1,
+          ),
+          // controller: searchController,
+          // onChanged: (String value) {
+          //   this.searchUsers(value);
+          //   if (value.length > 0) {
+          //     setState(() {
+          //       searching = true;
+          //     });
+          //   } else {
+          //     setState(() {
+          //       searching = false;
+          //     });
+          //   }
+          // },
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: () {})],
+      ),
+    );
+  }
+}
