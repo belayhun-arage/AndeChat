1850affcb2352cdc25884079c063219d41e88e7f
NAthyAlem <56716964+NAthyAlem@users.noreply.github.com>
2021-03-01 06:42:10 +0300

    adding Change password screen

---
+++ lib/screens/ChangePassword.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Part of Enawga',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Change Password"),
+          centerTitle: true,
+        ),
+        body: Container(
+            padding: EdgeInsets.symmetric(vertical: 25),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(0),
+                  height: 80,
+                  child: Card(
+                    child: Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: "Old Password",
+                              hintText: "Enter the Old password"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(0),
+                  height: 80,
+                  child: Card(
+                    child: Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: "New Password",
+                              hintText: "Enter the New password"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(0),
+                  height: 80,
+                  child: Card(
+                    child: Expanded(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: "Confirm Password",
+                              hintText: "confirm the New password"),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: FlatButton(
+                    child: Text(
+                      'Confirm',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    color: Colors.orange,
+                    textColor: Colors.black,
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            )));
+  }
+}
